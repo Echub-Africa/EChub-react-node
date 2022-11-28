1268978a1abe23bd70e29319c6dd7c84864b016a
Axunda <charlesemmanuelazunda@gmail.com>
2022-11-28 09:20:00 +0100

    first section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import Home from "./pages/Home.jsx"
 
 import './App.css';
-import "./hover.css";
+
 
 function App() {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,86 @@
 import React from 'react'
+import Person3Icon from '@mui/icons-material/Person3';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import Echub from "../assets/Echub Logo.svg"
 
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <a className="nav-link brand hvr-wobble-vertical" href="#">TechVillage</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Services</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="#">About Us</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="#">Contact Us</a>
-          </li>
+    
+  
+    <nav className="navbar navbar-expand-lg ">
+
+      <div className="container ">
+
+        <a className="navbar-brand me-2" href="https://mdbgo.com/">
+        <div className='brandName'>
+        <h1 className='brand'>Blue collar</h1>
+        <div className='logo'>
+          <img
+            src={Echub}
+            alt="MDB Logo"
+          />
+          <p>by</p>
+
+        </div>
+
+        </div>
           
-          <li className="nav-item">
-            <a className="nav-link active" href="#">FAQS</a>
-          </li>
-        </ul>
-        <form className="d-flex navForm" role="search">
-        <input className='navInput' type="text" placeholder='search'/>
-        <button className='navButton'>search</button>
-       
-          
-        </form>
+        </a>
+
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarButtonsExample"
+          aria-controls="navbarButtonsExample"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars"></i>
+        </button>
+
+
+        <div className="collapse navbar-collapse" id="navbarButtonsExample">
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Contact Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">FAQ'S</a>
+            </li>
+          </ul>
+
+
+          <div className="d-flex align-items-center">
+          <button type="button" className="btn btn-link px-3 me-2 bold">
+              Sign Up
+            </button>
+            <button type="button" className="btn btn-link px-3 me-2 bold">
+              Login
+            </button>
+            <button type="button" className="btn btn-primary me-3 bold">
+              Join the Team
+            </button>
+            
+          </div>
+        </div>
+
       </div>
-    </div>
-  </nav>
+
+    </nav>
+    
+
   )
 }
