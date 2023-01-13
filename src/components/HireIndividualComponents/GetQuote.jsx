53c8e911fdeff3146c632bb04d65cd5ab1986eba
Samuel Adex <adexsamuel6@gmail.com>
2023-01-13 18:38:00 +0100

    Completed the Hire Individual Page

---
+++ src/components/HireIndividualComponents/GetQuote.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const GetQuote = () => {
+    return (
+        <div className="quote">
+            <div className="container">
+                <div className="quote-header">
+                    <h1>HERE IS HOW TECH VILLAGE CAN HELP YOU TODAY </h1>
+                </div>
+                <div className="quote-btn">
+                    <button>Get Quote</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GetQuote;
---
+++ src/components/HireIndividualComponents/HireModal.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import Echub from '../../assets/Echub Logo.svg';
+
+
+// const HireModal = ({setIsOpen}) =>{
+//     return (
+//         <div className="hire-modal-overlay">
+//             <div className="modal">
+//                 <div className="close" onClick={()=> setIsOpen(false)}>x</div>
+//                 <div className="modal-icon">
+//                     <img src={Echub} alt="" />
+//                 </div>
+//                 <div className="hire-tech">
+//                     <div className="tech-header">
+//                         <h1>HIRE TECHNICIANS</h1>
+//                     </div>
+//                     tech-item   
+//                 </div>
+//                 <div className="team">
+
+//                 </div>
+//                 <div className="about">
+
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default HireModal;
