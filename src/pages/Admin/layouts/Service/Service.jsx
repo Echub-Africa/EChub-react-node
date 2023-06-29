61fc11840c734510cfac399b9b9a87a012cef57b
Samuel Adex <adexsamuel6@gmail.com>
2023-06-29 21:17:43 +0100

    made new changes to the Service.jsx file

--- src/pages/Admin/layouts/Service/Service.jsx
+++ src/pages/Admin/layouts/Service/Service.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from 'react';
 import {MdClose} from 'react-icons/md';
 import {useDispatch, useSelector} from 'react-redux';
